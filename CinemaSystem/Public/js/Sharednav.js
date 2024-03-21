fb5d6ddd3f7f172b5c21902ca6b05c8d36627383
TohmKench <123498558+TohmKench@users.noreply.github.com>
2024-03-21 17:58:15 +0000

    Add files via upload

--- CinemaSystem/Public/js/Sharednav.js
+++ CinemaSystem/Public/js/Sharednav.js
@@ -2,29 +2,32 @@ $("document").ready(function () {
     var isLoggedIn = localStorage.getItem('isLoggedIn') === 'true'; // Check if the user is logged in
     var userType = localStorage.getItem('userType'); // Get the user type
 
-    var loginLogoutLink = isLoggedIn 
-        ? '<li class="nav-item"><a class="nav-link text-white" href="logout.html">Logout</a></li>'
-        : '<li class="nav-item"><a class="nav-link text-white" href="logIn.html">Log in</a></li>';
+    
 
     var adminLink = userType === 'admin' 
         ? '<li class="nav-item"><a class="nav-link text-white" href="admin.html">Admin</a></li>'
         : '';
         
 
+        var loginLogoutLink = isLoggedIn 
+        ? '<li class="nav-item"><a class="nav-link text-white" href="logout.html">Logout</a></li>'
+        : '<li class="nav-item"><a class="nav-link text-white" href="logIn.html">Log in</a></li>';
+
     $("#myDiv").append(`
         <nav class="bg-secondary py-2">
             <div class="container">
                 <ul class="nav justify-content-center">
                     <li class="nav-item"><a class="nav-link text-white" href="home.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="movies.html">Movies</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="screenings.html">Screenings</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="ticket.html">Ticket</a></li>
-                    <li class="nav-item"><a class="nav-link text-white" href="ticketType.html">Ticket Type</a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="screeningsByDate.html">Today's Screenings</a></li>
-                    ${loginLogoutLink}
                     ${adminLink}
+                    ${loginLogoutLink}
                 </ul>
             </div>
-        </nav>`
+        </nav>
+        <footer class="bg-dark text-white py-3" style="position: absolute; bottom: 0; width: 100%; height: 60px;">
+            <div class="container text-center">
+                <p>&copy; 2024 Cinema System. All rights reserved.</p>
+            </div>
+        </footer>`
     );
 });
