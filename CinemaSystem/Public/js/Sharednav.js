94ab98fe12dae6fd1e70aa55df4270f2ba3d5717
TohmKench <123498558+TohmKench@users.noreply.github.com>
2024-03-21 17:25:07 +0000

    Update Sharednav.js
    Sharednav now appends different data if user is an Admin

--- CinemaSystem/Public/js/Sharednav.js
+++ CinemaSystem/Public/js/Sharednav.js
@@ -1,27 +1,30 @@
 $("document").ready(function () {
-
-
-    var isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';//Check if the user is logged in
+    var isLoggedIn = localStorage.getItem('isLoggedIn') === 'true'; // Check if the user is logged in
+    var userType = localStorage.getItem('userType'); // Get the user type
 
     var loginLogoutLink = isLoggedIn 
         ? '<li class="nav-item"><a class="nav-link text-white" href="logout.html">Logout</a></li>'
         : '<li class="nav-item"><a class="nav-link text-white" href="logIn.html">Log in</a></li>';
 
+    var adminLink = userType === 'admin' 
+        ? '<li class="nav-item"><a class="nav-link text-white" href="admin.html">Admin</a></li>'
+        : '';
+        
 
-$("#myDiv").append(`
-<nav class="bg-secondary py-2">
-<div class="container">
-    <ul class="nav justify-content-center">
-        <li class="nav-item"><a class="nav-link text-white" href="home.html">Home</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="movies.html">Movies</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="screenings.html">Screenings</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="ticket.html">Ticket</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="ticketType.html">Ticket Type</a></li>
-        <li class="nav-item"><a class="nav-link text-white" href="screeningsByDate.html">Today's Screenings</a></li>
-         ${loginLogoutLink}
-    </ul>
-</div>
-</nav>`
-);
-
+    $("#myDiv").append(`
+        <nav class="bg-secondary py-2">
+            <div class="container">
+                <ul class="nav justify-content-center">
+                    <li class="nav-item"><a class="nav-link text-white" href="home.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link text-white" href="movies.html">Movies</a></li>
+                    <li class="nav-item"><a class="nav-link text-white" href="screenings.html">Screenings</a></li>
+                    <li class="nav-item"><a class="nav-link text-white" href="ticket.html">Ticket</a></li>
+                    <li class="nav-item"><a class="nav-link text-white" href="ticketType.html">Ticket Type</a></li>
+                    <li class="nav-item"><a class="nav-link text-white" href="screeningsByDate.html">Today's Screenings</a></li>
+                    ${loginLogoutLink}
+                    ${adminLink}
+                </ul>
+            </div>
+        </nav>`
+    );
 });
