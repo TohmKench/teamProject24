a8c6ece49d0b44a679e763ddd7a4c47bff232e0e
TohmKench <123498558+TohmKench@users.noreply.github.com>
2024-04-11 14:49:46 +0100

    Add files via upload

--- CinemaSystem/Public/js/Sharednav.js
+++ CinemaSystem/Public/js/Sharednav.js
@@ -3,26 +3,28 @@ $("document").ready(function () {
     var userType = localStorage.getItem('userType'); // Get the user type
 
 
-    
-
+   
+        
     var adminLink = userType === 'admin' 
-        ? '<li class="nav-item"><a class="nav-link text-white" href="admin.html">Admin</a></li>'
-        : '';
+    ? '<li class="nav-item"><a class="nav-link text-white" href="admin.html">Admin</a></li>'
+    : '';
         
 
         var loginLogoutLink = isLoggedIn 
-        ? '<li class="nav-item"><a class="nav-link text-white" href="account.html">My Account</a></li>'
-        + '<li class="nav-item"><a class="nav-link text-white" href="logout.html">Logout</a></li>'
-        : '<li class="nav-item"><a class="nav-link text-white" href="logIn.html">Log in</a></li>';
+        ? '<li class="nav-item" style="margin-left: 605px;"><a class="nav-link text-white" href="account.html">My Account</a></li>'
+        + '<li class="nav-item" style="margin-left: auto;"><a class="nav-link text-white" href="logout.html">Logout</a></li>'
+        : '<li class="nav-item" style="margin-left: auto;"><a class="nav-link text-white" href="logIn.html">Log in</a></li>';
 
     var pageTitle = document.title; // Get the title of the page
 
     $("#myDiv").append(`
         <nav class="bg-secondary py-2" style="position: fixed; top: 0; width: 100%; z-index: 100;">
             <div class="container">
-                <ul class="nav justify-content-center">
+                <ul class="nav justify-content-left">
+                    <li class="nav-item"><a class="nav-link text-white" href="home.html"><img src="icons/home.png" onclick="window.location.href='home.html'" style="width: 30px; height: 25px;"></a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="home.html">Home</a></li>
                     <li class="nav-item"><a class="nav-link text-white" href="screeningsByDate.html">Today's Screenings</a></li>
+                    <li class="nav-item"><a class="nav-link text-white" href="map.html">About Us</a></li>
                     ${adminLink}
                     ${loginLogoutLink}
                 </ul>
@@ -39,6 +41,6 @@ $("document").ready(function () {
                 <p>&copy; 2024 Cinema System. All rights reserved.</p>
             </div>
         </footer>`
-    );
+    )
 
 });
