9dc051f29ebd260ecf5201ace6b8d9eee621fc2c
JasonM <124704852+jasongr7@users.noreply.github.com>
2024-04-21 16:26:21 +0100

    Update db.js

--- CinemaSystem/model/db.js
+++ CinemaSystem/model/db.js
@@ -545,10 +545,9 @@ exports.updatePassword = function(req, res) {
 // Retrieve screenings by a specific date
 exports.getScreeningsByDate = function(req, res, date) {
   var query = `
-    SELECT screen.*, movie.title AS movieTitle, theatre.name AS theatreName
-    FROM screen
-    INNER JOIN movie ON screen.movieId = movie.movieId
-    INNER JOIN theatre ON screen.theatreId = theatre.theatreId
+    SELECT screen.*, movie.title AS movieTitle
+    FROM screen 
+    JOIN movie ON screen.movieId = movie.movieId
     WHERE DATE(startTime) = ?`;
   connection.query(query, [date], function(err, rows) {
     if (err) {
