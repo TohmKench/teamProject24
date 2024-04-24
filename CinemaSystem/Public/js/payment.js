7351f53cddf8819401d9e3b213d8797bde040c27
skiiotb <124704117+skiiotb@users.noreply.github.com>
2024-04-24 12:19:25 +0100

    Update payment.js

--- CinemaSystem/Public/js/payment.js
+++ CinemaSystem/Public/js/payment.js
@@ -39,13 +39,16 @@
             createBooking(totalCost, bookingDetails);
         }
 
-        function createBooking(totalCost, bookingDetails) {
+        function createBooking(totalCost, bookingDetails, currentSeats) {
             
             var date = bookingDetails.bookingDate;
             var email = bookingDetails.emailAddress;
             var seats = bookingDetails.totalSeats;
+            var screenId = bookingDetails.screenId;
+            var selectedTicketTypes = bookingDetails.selectedTicketTypes;
+            var currentSeats = localStorage.getItem('currentSeats');
         
-            $.post("http://localhost:3000/createBooking", {
+            /*$.post("http://localhost:3000/createBooking", {
 
                 "bookingDate": date,
                 "totalCost": totalCost,
@@ -56,6 +59,36 @@
                 alert("Booking created successfully with ID: " + data.bookingId);
                 // Redirecting to payment confirmation page
                 window.location.href = "paymentConfirmation.html";
-            });
+            });*/
+
+            $.post("http://localhost:3000/createBooking", {"bookingDate": date, "totalCost": totalCost, "seats": seats, "emailAddress" : email}, function(data){
+                        console.log(data);
+                        bookingId = data.bookingId; // Retrieve the bookingId from the response data
+                        alert("Booking created successfully with ID: " + data.bookingId);
+                        //window.location.href = "home.html";
+                        //createTickets(bookingId, ticketTypes);
+                        var seatsRemaining = parseInt(currentSeats) - seats;
+                        console.log(seatsRemaining);
+                        $.getJSON("http://localhost:3000/screenings/" + screenId, function(data) {
+                            console.log(data);
+                            $.post("http://localhost:3000/updateScreening", {"movieId" : data[0].movieId, "startTime" : data[0].startTime, "endTime" : data[0].endTime, "seatsRemaining" : seatsRemaining, "screenId" : data[0].screenId}, function(data){
+                                console.log(data);
+                                alert("Screening updated successfully with id: " + data.screenId);   
+                            });
+                            selectedTicketTypes.forEach(function(ticketType) {
+                                console.log(ticketType);
+                                for(let i = 0; i < ticketType.quantity; i++) {
+                                    $.post("http://localhost:3000/createTicket", {"bookingId": bookingId, "ticketType": ticketType.typeName, "screenId": ticketType.screenId}, function(data){
+                                        console.log(data);
+                                        alert("Ticket created successfully with ID: " + data.ticketId);
+                                    });
+                                }
+                            });   
+                            window.location.href = "paymentConfirmation.html";  
+                        });
+                                         
+                    }); 
+            
         }
     });
+
