ae3d3dfc7e95a12a1d1ce0fc7722b5aa48cad70c
skiiotb <124704117+skiiotb@users.noreply.github.com>
2024-04-24 12:18:46 +0100

    Update bookNow.js

--- CinemaSystem/Public/js/bookNow.js
+++ CinemaSystem/Public/js/bookNow.js
@@ -58,10 +58,13 @@ $(document).ready(function(){
 
         // Storing total cost and booking details in local storage for payment page
         localStorage.setItem('totalCost', totalCost);
+        localStorage.setItem('currentSeats', currentSeats);
         localStorage.setItem('bookingDetails', JSON.stringify({
+            screenId: screenId,
             bookingDate: startDate,
             totalSeats: totalSeats,
-            emailAddress: emailAddress
+            emailAddress: emailAddress,
+            selectedTicketTypes: selectedTicketTypes
         }));
             
             $.getJSON("http://localhost:3000/ticketType" , function(data) {
@@ -69,7 +72,7 @@ $(document).ready(function(){
 
                 console.log(totalCost);
                 if(currentSeats > totalSeats){
-                    $.post("http://localhost:3000/createBooking", {"bookingDate": startDate, "totalCost": totalCost, "seats": totalSeats, "emailAddress" : emailAddress}, function(data){
+                    /*$.post("http://localhost:3000/createBooking", {"bookingDate": startDate, "totalCost": totalCost, "seats": totalSeats, "emailAddress" : emailAddress}, function(data){
                         console.log(data);
                         bookingId = data.bookingId; // Retrieve the bookingId from the response data
                         alert("Booking created successfully with ID: " + data.bookingId);
@@ -81,17 +84,18 @@ $(document).ready(function(){
                             console.log(data);
                             $.post("http://localhost:3000/updateScreening", {"movieId" : data[0].movieId, "startTime" : data[0].startTime, "endTime" : data[0].endTime, "seatsRemaining" : seatsRemaining, "screenId" : data[0].screenId}, function(data){
                                 console.log(data);
-                                alert("Screening updated successfullywith id: " + data.screenId);   
+                                alert("Screening updated successfully with id: " + data.screenId);   
                             });
-                            selectedTicketTypes.forEach(function(ticketType) {
+                                 selectedTicketTypes.forEach(function(ticketType) {
                                 $.post("http://localhost:3000/createTicket", {"bookingId": bookingId, "ticketType": ticketType.typeName, "screenId": ticketType.screenId}, function(data){
                                     console.log(data);
                                     alert("Ticket created successfully with ID: " + data.ticketId);
                                 });
                             });  
                         });
-                                              
-                    }); 
+                                         
+                    }); */
+                    window.location.href = "payment.html";
                 }
                 else{
                     alert("Not enough seats available");
@@ -101,3 +105,4 @@ $(document).ready(function(){
         });
     });
 });
+
